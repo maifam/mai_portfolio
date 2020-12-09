ff95cf6553d417079a34860e79b31d7000f25030
Mai Pham <maitphm@gmail.com>
2020-12-08 19:18:07 -0500

    updated

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Typed from "react-typed"
+import {SocialIcon} from 'react-social-icons'
+import {Link} from "react-scroll"
+
+function Header(){
+return(
+  <div id = "home" className = "header-wrapper">
+      <div className = "main-info">
+           <h1>Mai Pham</h1>
+          {/* <h2>Software Engineer </h2> */}
+          <Typed 
+            className = "typed-text"
+            strings = {["Software Engineer", "Javascript", "React", "Redux", "Ruby on Rails", "HTML", "CSS", "PostgreSQL", "SQLite", "Git", "Heroku", "Netlify" ]}
+            typeSpeed={50}
+            backSpeed={50}
+            loop
+          />
+          <Link smooth = {true} to="contact" className= "btn-main-info" href="#">Contact Me</Link>
+
+      </div>
+  </div>
+)
+}
+
+export default Header;
