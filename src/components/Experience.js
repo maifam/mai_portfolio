fd7d2663552f62e0112df12582f9034487921751
Mai Pham <maitphm@gmail.com>
2021-04-09 18:49:59 -0400

    updated

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -31,7 +31,6 @@ function Experience() {
                                     behavioral health for an average of 55 clients
                         <br></br>● Provided feedback, recommendations and suggestions to staff 
                                     ranging from behavioral outbursts of clients and daily facility operations 
-                        <br></br>● 
                         <br></br>●  Spearheaded safety committee meetings on relapse and self-harm 
                                     prevention and awareness in clients behaviors
                     </div>
