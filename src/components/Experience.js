69d9ae0f636d579ad1c9de2429e3e3243677d567
Mai Pham <maitphm@gmail.com>
2021-04-26 18:34:26 -0400

    updated

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -39,7 +39,6 @@ function Experience() {
                         <h3> Flatiron School(Winter 2020 - Spring 2021) </h3>    
                         <p>
                         <br></br>● Part of a four-person team tasked with providing model driven performance for roughly 15,000 accounts and over $6 billion dollars in assets under management 
-                        <br></br>● Part of team that led in-house implementation of Vestmark, a portfolio management system now used by all financial advisors at Oppenheimer (this entailed testing daily functionalities such as trade generation, contributions, withdrawals, and writing internal procedures for such tasks) 
                         <br></br>● Identify Vestmark system shortcomings and provide desired results to improve system functionalities 
                         <br></br>● Communicate with client services to understand individual investors’ objectives and risk profiles 
                         <br></br>● Daily responsibilities include pre-trade review for existing clients, trade implementation for new accounts, investment allocation changes, and cash and security contributions and withdrawals 
