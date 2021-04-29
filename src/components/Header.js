3ba33cea17a932a5f4857983731326a9e9d19df3
Mai Pham <maitphm@gmail.com>
2021-04-29 16:38:34 -0400

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ return(
           {/* <h2>Software Engineer </h2> */}
           <Typed 
             className = "typed-text"
-            strings = {["Software Engineer", "Javascript", "React" , "Redux" , "Ruby on Rails" , "HTML"]}
+            strings = {["Software Engineer", "Javascript", "React" , "Redux" , "Ruby on Rails" , "HTML" , "CSS" ]}
             typeSpeed={50}
             backSpeed={50}
             loop
