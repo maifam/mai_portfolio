eb27170dfa3be1c4d86351e45e690376afc93e96
Mai Pham <maitphm@gmail.com>
2021-05-21 16:34:31 -0400

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ return(
             backSpeed={50}
             loop
           />
-          <Link smooth = {true} to="contact" className= "btn-main-info" href="#">Contac</Link>
+          <Link smooth = {true} to="contact" className= "btn-main-info" href="#">Contact</Link>
 
       </div>
   </div>
