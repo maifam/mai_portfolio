4b8f51110bd1858798cc3abba1b2748ef6ff7dd0
Mai Pham <maitphm@gmail.com>
2021-07-15 18:27:42 -0400

    github

--- src/components/Links.js
+++ src/components/Links.js
@@ -3,6 +3,7 @@ function Links() {
       <div>
           <ul class="icons">
 						<li><a href="https://www.linkedin.com/in/maitphm" target='_blank' class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
+						<li><a href="https://github.com/maifam" target='_blank' class="icon brands fa-github"><span class="label">Github</span></a></li>
 						
 					</ul>
       </div>
