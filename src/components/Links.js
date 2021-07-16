813dc5a9173bfb79a19e37e4c2b8badbf80b2ce4
Mai Pham <maitphm@gmail.com>
2021-07-16 18:28:13 -0400

    angelco

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,7 +4,8 @@ function Links() {
           <ul class="icons">
 						<li><a href="https://www.linkedin.com/in/maitphm" target='_blank' class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
 						<li><a href="https://github.com/maifam" target='_blank' class="icon brands fa-github"><span class="label">Github</span></a></li>
-						
+						<li><a href="https://angel.co/u/maitphm" target='_blank' class="icon brands fa-angellist"><span class="label">Medium</span></a></li>
+
 					</ul>
       </div>
     );
